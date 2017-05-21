71c36f4d7662dd19c4f573cbe036de3e0925ed8d
Alejowilches0112 <cawilchesfe@poligran.edu.co>
2017-05-21 12:42:24 -0500

    Servicios REST

---
+++ Buscador_Bancos/REST/UsuarioDB.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UsuarioDB
+ *
+ * @author ILP
+ */
+class UsuarioDB {
+    //put your code here
+}
