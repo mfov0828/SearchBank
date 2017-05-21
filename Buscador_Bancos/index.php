04d30a9c42ca74c635c5b0694ff7151548e54587
Alejowilches0112 <cawilchesfe@poligran.edu.co>
2017-05-21 13:44:05 -0500

    Index

--- Buscador_Bancos/index.php
+++ Buscador_Bancos/index.php
@@ -1,19 +1,26 @@
-<!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <meta charset="UTF-8">
         <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="Style.css"/>
     </head>
     <body>
+        <form id="loginform" method="" action="">
+        <input type="text" class="input" placeholder="E-mail" /> 
+        <input type="password" class="input" placeholder="Password" />
+        <input type="submit" class="loginbutton" value="SIGN IN" />
+        </form>
+        <a href="#">Click here for new users</a>
     <?php
-        require_once "/REST/UsuarioApi.php";    
-        $UsuarioApi = new UsuarioApi();
-        $UsuarioAPo->API();
+        //require_once "/REST/UsuarioApi.php";    
+        //$UsuarioApi = new UsuarioApi();
+        //$UsuarioAPo->API();
     ?>
     </body>
 </html>
